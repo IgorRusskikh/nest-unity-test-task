d950020f552dd1018cccea0cffa0b6ad79d736a8
IgorRusskikh <russkikh200@mail.ru>
2025-05-20 20:11:48 +0700

    feat: add interfaces for api data and prepared api data

---
+++ src/common/interfaces/api-response-data.interface.ts
@@ -0,0 +1,7 @@
+export interface ApiResponseData {
+  ObjectName: string;
+  Year: number;
+  Month: number;
+  Plan: number;
+  Fact: number;
+}
---
+++ src/common/interfaces/prepared-data.interface.ts
@@ -0,0 +1,6 @@
+import { ApiResponseData } from './api-response-data.interface';
+
+export interface PreparedData {
+  ObjectName: string;
+  data: ApiResponseData[];
+}
